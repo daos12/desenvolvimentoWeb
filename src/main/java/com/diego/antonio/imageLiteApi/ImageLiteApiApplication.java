f197c4ce44dc18193b7e6e4389389274ddf70d90
daos12 <daos_ptc@hotmail.com>
2025-04-13 01:15:34 -0300

    Commit Inicial

---
+++ src/main/java/com/diego/antonio/imageLiteApi/ImageLiteApiApplication.java
@@ -0,0 +1,13 @@
+package com.diego.antonio.imageLiteApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageLiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageLiteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/antonio/imageLiteApi/ImageLiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.antonio.imageLiteApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageLiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
